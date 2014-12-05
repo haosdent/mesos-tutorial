346536899a46787f51db2fc8ccad46b7c6991d35
haosdent <haosdent@gmail.com>
2014-12-05 11:15:03 +0800

    Implement LaunchTask

--- src/executor.go
+++ src/executor.go
@@ -24,3 +24,31 @@ func (self *ExampleExecutor) Registered(driver exec.ExecutorDriver, execInfo *me
 func (self *ExampleExecutor) Reregistered(driver exec.ExecutorDriver, slaveInfo *mesos.SlaveInfo) {
     fmt.Println("Call ExampleExecutor.Re-registered")
 }
+
+func (self *ExampleExecutor) Disconnected(exec.ExecutorDriver) {
+    fmt.Println("Call ExampleExecutor.Disconnected")
+}
+
+func (self *ExampleExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *mesos.TaskInfo) {
+    fmt.Println("Call ExampleExecutor.LaunchTask")
+
+    runStatus := &mesos.TaskStatus{
+        TaskId: taskInfo.GetTaskId(),
+        State:  mesos.TaskState_TASK_RUNNING.Enum(),
+    }
+    _, err := driver.SendStatusUpdate(runStatus)
+    if err != nil {
+        fmt.Println("Got error in ExampleExecutor.LaunchTask: ", err)
+    }
+
+    exec.tasksLaunched++
+
+    finStatus := &mesos.TaskStatus{
+        TaskId: taskInfo.GetTaskId(),
+        State:  mesos.TaskState_TASK_FINISHED.Enum(),
+    }
+    _, err = driver.SendStatusUpdate(finStatus)
+    if err != nil {
+        fmt.Println("Got error in ExampleExecutor.LaunchTask: ", err)
+    }
+}
