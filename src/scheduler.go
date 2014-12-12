90693ac073c95df6d2130af4ccb59e4da59764f3
haosdent <haosdent@gmail.com>
2014-12-12 15:08:40 +0800

    Add Registered to scheduler

---
+++ src/scheduler.go
@@ -0,0 +1,25 @@
+package main
+
+type ExampleScheduler struct {
+    executor      *mesos.ExecutorInfo
+    tasksLaunched int
+    tasksFinished int
+    totalTasks    int
+}
+
+func NewExampleScheduler(exec *mesos.ExecutorInfo) *ExampleScheduler {
+    total, err := strconv.Atoi(*taskCount)
+    if err != nil {
+        total = 5
+    }
+    return &ExampleScheduler{
+        executor:      exec,
+        tasksLaunched: 0,
+        tasksFinished: 0,
+        totalTasks:    total,
+    }
+}
+
+func (self *ExampleScheduler) Registered(driver sched.SchedulerDriver, frameworkId *mesos.FrameworkID, masterInfo *mesos.MasterInfo) {
+    fmt.Printfln("Call ExampleScheduler.Registered")
+}
